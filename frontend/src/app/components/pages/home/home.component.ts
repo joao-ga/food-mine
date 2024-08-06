c1d494b148bfbd8dd4b6534d64a716b76e9ddcca
joaogabiazon <jbiazonferreira@gmail.com>
2024-08-06 20:51:09 -0300

    Tags component

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -6,11 +6,12 @@ import { Food } from '../../../shared/models/Food';
 import { RatingModule } from 'ngx-bootstrap/rating';
 import { FormsModule } from '@angular/forms';
 import { SearchComponent } from "../../partials/search/search.component";
+import { TagsComponent } from '../../partials/tags/tags.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, RatingModule, FormsModule, SearchComponent], 
+  imports: [CommonModule, RouterModule, RatingModule, FormsModule, SearchComponent, TagsComponent], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -22,6 +23,8 @@ export class HomeComponent {
     activatedRoute.params.subscribe((params) => {
       if(params['searchTerm']) {
         this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+      } else if(params['tag']) {
+        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
       } else {
         this.foods = foodService.getAll();
       }
