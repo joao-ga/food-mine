75a1699d5f8a47740b1ff46c43f48f5c5b7998b7
joaogabiazon <jbiazonferreira@gmail.com>
2024-08-06 21:18:42 -0300

    Page foodd done

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
     {path:'search/:searchTerm', component: HomeComponent},
-    {path: 'tag/:tag', component: HomeComponent}
+    {path: 'tag/:tag', component: HomeComponent},
+    {path: 'food/:id', component:FoodPageComponent}
 ];
 
 @NgModule({
