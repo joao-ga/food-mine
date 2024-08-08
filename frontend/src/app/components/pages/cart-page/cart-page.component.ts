035039bec01fd1187163f7284752caa5e968f8e7
joaogabiazon <jbiazonferreira@gmail.com>
2024-08-07 21:32:20 -0300

    Add cart page service and title component

---
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Cart } from '../../../shared/models/Cart';
+import { CartService } from '../../../services/cart.service';
+import { CartItem } from '../../../shared/models/CartItem';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from "../../partials/title/title.component";
+
+@Component({
+  selector: 'app-cart-page',
+  standalone: true,
+  imports: [RouterModule, CommonModule, TitleComponent],
+  templateUrl: './cart-page.component.html',
+  styleUrl: './cart-page.component.css'
+})
+export class CartPageComponent {
+
+  cart!: Cart;
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable()
+    .subscribe((cart) => {
+      this.cart = cart
+    })
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString:string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
+
+}
--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/Food';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
 import { CommonModule } from '@angular/common';
 import { RatingModule } from 'ngx-bootstrap/rating';
 import { FormsModule } from '@angular/forms';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -16,11 +17,24 @@ import { FormsModule } from '@angular/forms';
 export class FoodPageComponent {
   food!: Food;
 
-  constructor(activatedRoute:ActivatedRoute, foodService: FoodService) {
+  constructor(
+    activatedRoute:ActivatedRoute, 
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+   ) {
+
     activatedRoute.params.subscribe((params) => {
       if(params['id']) {
           this.food = foodService.getFoodById(params['id']);
       }
     })
+
   }
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  } 
+
 }
